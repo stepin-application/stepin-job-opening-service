7bc0de066fc1bb0b36f9c11c196d67a232291d2f
Niko7895 <nicolas.chalumeau78@gmail.com>
2026-01-22 23:20:19 +0100

    Remplacement du repository In-Memory par JPA

--- src/main/java/com/stepin/jobopening/repository/JobOpeningRepository.java
+++ src/main/java/com/stepin/jobopening/repository/JobOpeningRepository.java
@@ -1,19 +1,14 @@
 package com.stepin.jobopening.repository;
 
 import com.stepin.jobopening.domain.JobOpening;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface JobOpeningRepository {
-    JobOpening save(JobOpening jobOpening);
-
-    Optional<JobOpening> findById(UUID id);
-
+public interface JobOpeningRepository extends JpaRepository<JobOpening, UUID> {
     List<JobOpening> findByCampaignIdAndCompanyId(UUID campaignId, UUID companyId);
 
     Optional<JobOpening> findByIdAndCampaignIdAndCompanyId(UUID id, UUID campaignId, UUID companyId);
-
-    void delete(JobOpening jobOpening);
 }
