17a6d6ac96e1d78465945ea14cfa0ea0390f2ed5
Niko7895 <nicolas.chalumeau78@gmail.com>
2026-01-22 23:19:10 +0100

    Passage du modele domain en entite JPA

--- src/main/java/com/stepin/jobopening/domain/JobOpening.java
+++ src/main/java/com/stepin/jobopening/domain/JobOpening.java
@@ -1,16 +1,38 @@
 package com.stepin.jobopening.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
+@Entity
+@Table(name = "job_opening")
 public class JobOpening {
+    @Id
+    @Column(name = "job_opening_id")
     private UUID id;
+
+    @Column(name = "campaign_id", nullable = false)
     private UUID campaignId;
+
+    @Column(name = "company_id", nullable = false)
     private UUID companyId;
+
+    @Column(name = "title", nullable = false)
     private String title;
+
+    @Column(name = "description", nullable = false)
     private String description;
+
+    @Column(name = "requirements")
     private String requirements;
+
+    @Column(name = "created_at", nullable = false)
     private OffsetDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
     private OffsetDateTime updatedAt;
 
     public JobOpening() {
